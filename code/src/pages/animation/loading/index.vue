a702e1cf73fba8cc51a861906929d6f835b826a9
evan-zl <evan2016@vip.qq.com>
2024-01-25 10:04:07 +0800

    fix:loading

--- code/src/pages/animation/loading/index.vue
+++ code/src/pages/animation/loading/index.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="page">
+  <div :class="$style.page">
     
-    <div class="demo">
-      <div class="dashed">
+    <div :class="$style.demo">
+      <div :class="$style.dashed">
         <span v-for="i in 15" :style="{ '--i': i }"></span>
       </div>
     </div>
 
 
-    <div class="demo">
-      <div class="cylinder">
+    <div :class="$style.demo">
+      <div :class="$style.cylinder">
         <div class="bar" v-for="_i in 10">
           <i v-for="_j in 20"></i>
         </div>
+      </div>
+    </div>
+
+    <div :class="[ $style.demo, $style.demo3 ]">
+      <i></i><i></i><i></i><i></i><i></i>
+    </div>
+
+    <div :class="[ $style.demo, $style.demo4 ]">
+      <i></i>
+    </div>
+
+    <div :class="[ $style.demo, $style.demo5 ]">
+      <i></i><i></i><i></i>
+    </div>
+
+    <div :class="[ $style.demo, $style.demo6 ]">
+      <i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i>
+    </div>
+    
+    <div :class="[ $style.demo ]">
+      <div :class="$style.demo7">
+        <i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i>
+      </div>
+    </div>
+
+    <div :class="[ $style.demo ]">
+      <div :class="$style.demo8">
+        <i></i><i></i><i></i><i></i>
       </div>
     </div>
 
@@ -21,6 +49,6 @@
 
 <script lang="ts" setup>
 </script>
-  
-<style lang="scss" src="./style.scss" scoped>
+
+<style lang="scss" src="./style.scss" module>
 </style>
