64f5d0b4177a0c1d769660693c335ba335e68f0b
evan-zl <evan2016@vip.qq.com>
2024-09-03 17:57:22 +0800

    fix:mock 地址修正

--- code/src/config/index.ts
+++ code/src/config/index.ts
@@ -85,6 +85,7 @@ const apiFormat = (obj: any, key: any, isFirst: Boolean = false) => {
   })
 }
 apiFormat(Api, null, true)
+console.log(Api)
 
 const install = function (app: any) {
   app.config.globalProperties.$api = Api
--- code/src/config/mock/index.ts
+++ code/src/config/mock/index.ts
@@ -5,8 +5,9 @@ import user from './user'
 import test from './test'
 import monitor from './monitor'
 import d3 from './d3'
+import echarts from './echarts'
 
-const mock: Array<any> = [...user, ...test, ...monitor, ...d3].map(item => {
+const mock: Array<any> = [...user, ...test, ...monitor, ...d3, ...echarts].map(item => {
   item.url = '/mock' + item.url
   return item
 })
