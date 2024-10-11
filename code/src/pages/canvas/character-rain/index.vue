0323caf99c761b9d2985826e11beb5d1b590d493
evan-zl <evan2016@vip.qq.com>
2024-10-11 14:37:21 +0800

    fix:添加字符

--- code/src/pages/canvas/character-rain/index.vue
+++ code/src/pages/canvas/character-rain/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="page" ref="page">
-
     <canvas ref="canvas"></canvas>
-
   </div>
 </template>
 
@@ -17,60 +15,62 @@ let charIndex: Array<number> = []
 // 初始化
 const initCanvas = () => {
   const cvs = canvas.value
-  const ctx = cvs.getContext( '2d' )
+  const ctx = cvs.getContext('2d')
 
   // 设备缩放比例
   const devicePixelRatio = window.devicePixelRatio
   canvas.value.height = page.value.clientHeight * devicePixelRatio
   canvas.value.width = page.value.clientWidth * devicePixelRatio
   fontSize = 15 * devicePixelRatio
-  ctx.font = `${ fontSize }px "Roboto Mono"`
-  columnCont = Math.floor( cvs.width / fontSize )
-  charIndex = new Array( columnCont ).fill( 0 )
+  ctx.font = `${fontSize}px "Roboto Mono"`
+  columnCont = Math.floor(cvs.width / fontSize)
+  charIndex = new Array(columnCont).fill(0)
 
   draw()
-  clearInterval( timer )
-  timer = setInterval( draw, 50 )
+  clearInterval(timer)
+  timer = setInterval(draw, 50)
 }
 
 // 随机字符
 const getRandomChar = () => {
-  const str = 'qwertyuiopasdfghjklzxcvbnm1234567890', len = str.length
-  const s = Math.floor( Math.random() * len)
-  return str[ s ]
+  const str = 'qwertyuiopasdfghjklzxcvbnm1234567890`!@#$%^&*()_+-[]|',
+    len = str.length
+  const s = Math.floor(Math.random() * len)
+  return str[s]
 }
 
 const draw = () => {
   const cvs = canvas.value
-  const ctx = cvs.getContext( '2d' )
+  const ctx = cvs.getContext('2d')
   ctx.fillStyle = 'rgba(0,0,0,.1)'
-  ctx.fillRect( 0, 0, cvs.width, cvs.height )
+  ctx.fillRect(0, 0, cvs.width, cvs.height)
 
   ctx.fillStyle = '#6BE445'
   ctx.textAlign = 'left'
   ctx.textBaseline = 'top'
-  for( let i = 0; i < columnCont; i ++ ) {
+  for (let i = 0; i < columnCont; i++) {
     const char = getRandomChar()
-    const x = i * fontSize, y = charIndex[ i ] * fontSize
-    ctx.fillText( char, x, y )
-    if ( y > cvs.height && Math.random() > 0.99 ) {
-      charIndex[ i ] = 0
+    const x = i * fontSize,
+      y = charIndex[i] * fontSize
+    ctx.fillText(char, x, y)
+    if (y > cvs.height && Math.random() > 0.99) {
+      charIndex[i] = 0
     } else {
-      charIndex[ i ]++
+      charIndex[i]++
     }
   }
 }
 
-onMounted( () => {
+onMounted(() => {
   initCanvas()
-  window.addEventListener( 'resize', initCanvas )
-} )
-onBeforeUnmount( () => {
-  clearInterval( timer )
-  window.removeEventListener( 'resize', initCanvas )
-} )
+  window.addEventListener('resize', initCanvas)
+})
+onBeforeUnmount(() => {
+  clearInterval(timer)
+  window.removeEventListener('resize', initCanvas)
+})
 </script>
-  
+
 <style lang="scss" scoped>
 .page {
   overflow: hidden;
