a247ea2256e841bc86204660c99604783ad681fa
evan-zl <evan2016@vip.qq.com>
2024-10-11 14:39:35 +0800

    fix:限制缩放距离

--- code/src/pages/d3/scene/device/data.ts
+++ code/src/pages/d3/scene/device/data.ts
@@ -57,7 +57,7 @@ export const getPageOpts = (): {} & Omit<
   },
   controls: {
     screenSpacePanning: false,
-    maxDistance: 5000,
+    maxDistance: 1500,
     maxPolarAngle: Math.PI * 0.46
   },
   grid: {
