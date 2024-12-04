688f2282a84dc84838b78264ded6c4db3d0b441c
evan-zl <evan2016@vip.qq.com>
2024-12-04 09:28:52 +0800

    fix:移除无用 ts 声明

--- code/src/pages/d3/scene/station/index.d.ts
+++ code/src/pages/d3/scene/station/index.d.ts
@@ -1,4 +1,4 @@
-import type { XYZ } from '.three-scene/types/model'
+import type { XYZ } from 'three-scene/types/model'
 import type { Colors, ColorObject } from 'three-scene/types/color'
 
 export declare interface Config {
@@ -6,17 +6,6 @@ export declare interface Config {
   to: XYZ
   // 场景中心点/相机聚焦位置
   target: XYZ
-  // 楼层展开模式
-  // UD -> up-down | BA -> before-after
-  floorExpandMode: 'UD' | 'BA'
-  // 楼层展开间距
-  floorExpandMargin: number
-  // 楼层展开后隐藏其他模型
-  floorExpandHiddenOther: boolean
-  // 楼层展开的 索引(楼层类型列表索引)
-  floorExpandIndex: number
-  // 楼层展开是否改变视角
-  floorExpandChangeViewAngle: boolean
   // 返回
   back: Function
   // 加载
