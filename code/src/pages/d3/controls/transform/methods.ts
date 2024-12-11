ddf8ed02141dfd6f999b78c2dc63aec6262d9711
evan-zl <evan2016@vip.qq.com>
2024-12-11 18:09:48 +0800

    fix:添加 控制按钮

--- code/src/pages/d3/controls/transform/methods.ts
+++ code/src/pages/d3/controls/transform/methods.ts
@@ -41,9 +41,44 @@ export class NewThreeScene extends ThreeScene {
 
   addGui() {
     const gui = this.gui
-    const ctr = this.controls
-    console.log(ctr)
-    // gui.add(ctr, 'movementSpeed', 0.1, 1000).name('移动速度(方向按键、鼠标左右)')
+    const control = this.transformControls
+    const params = {
+      w: () => {
+        control.setMode('translate')
+      },
+      e: () => {
+        control.setMode('rotate')
+      },
+      r: () => {
+        control.setMode('scale')
+      },
+
+      x: false,
+      y: false,
+      z: false,
+
+      add: () => {
+        control.setSize(control.size + 0.1)
+      },
+      sub: () => {
+        control.setSize(Math.max(control.size - 0.1, 0.1))
+      },
+      space: false
+    }
+    const group = gui.addFolder('键盘快捷键')
+
+    group.add(params, 'w').name('“W”切换坐标辅助)')
+    group.add(params, 'e').name('“E”切换旋转辅助)')
+    group.add(params, 'r').name('“R”切换缩放辅助)')
+
+    group.add(control, 'showX').name('“X”坐标系 x 轴隐显)')
+    group.add(control, 'showY').name('“Y”坐标系 y 轴隐显)')
+    group.add(control, 'showZ').name('“Z”坐标系 z 轴隐显)')
+
+    group.add(params, 'add').name('“+”,"="坐标系放大)')
+    group.add(params, 'sub').name('“-”,"_"坐标系缩小)')
+    group.add(control, 'enabled').name('“空格”坐标系禁/启用)')
+
     gui.domElement.style = 'position: absolute; top: 0px; right: 0px'
     this.container.parentNode?.appendChild(gui.domElement)
   }
@@ -89,7 +124,6 @@ export class NewThreeScene extends ThreeScene {
   onKeydown(e) {
     console.log(e, this)
     const control = this.transformControls
-    console.log(control)
     switch (e.key) {
       case 'w':
         control.setMode('translate')
