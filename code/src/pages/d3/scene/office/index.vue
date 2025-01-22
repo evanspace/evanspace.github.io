98b2bb5eacddc0c3669570b5312931eee5243933
evan-zl <evan2016@vip.qq.com>
2025-01-22 18:18:11 +0800

    fix:模型更新

--- code/src/pages/d3/scene/office/index.vue
+++ code/src/pages/d3/scene/office/index.vue
@@ -129,7 +129,7 @@ const { progress, loadModels, getModel } = Hooks.useModelLoader({
     cache: true,
     dbName: 'THREE__OFFICE__DB',
     tbName: 'TB',
-    version: 57
+    version: 59
   }
 })
 
