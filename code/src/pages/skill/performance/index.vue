2790f84f6b98ad465a57eee825c00dc6bd2d8389
evan-zl <evan2016@vip.qq.com>
2025-03-04 09:22:57 +0800

    fix:添加动画

--- code/src/pages/skill/performance/index.vue
+++ code/src/pages/skill/performance/index.vue
@@ -5,10 +5,15 @@
         <el-input-number v-model="count" :min="5000"></el-input-number>
         个元素
       </div>
+      <div class="pt-sm">当前第：{{ current }} 个</div>
       <div class="pt-sm">耗时：{{ time }} ms</div>
       <br />
       <el-button type="primary" @click="onAdd">渲染无优化</el-button>
       <el-button type="primary" @click="onPerAdd">渲染优化</el-button>
+
+      <div :class="$style.animate">
+        <div :class="$style.dot"></div>
+      </div>
     </div>
 
     <div ref="containerRef" class="f-x o-a" style="font-size: 14px"></div>
@@ -18,7 +23,8 @@
 <script lang="ts" setup>
 const containerRef = ref()
 
-const count = ref(100_000)
+const count = ref(50_000)
+const current = ref(0)
 const time = ref(0)
 
 const onAdd = () => {
@@ -30,7 +36,7 @@ const onAdd = () => {
   for (let i = 0; i < count.value; i++) {
     const div = document.createElement('div')
     div.innerHTML = String(i + 1)
-
+    current.value = i + 1
     dom.appendChild(div)
   }
   time.value = Date.now() - t
@@ -50,8 +56,9 @@ const onPerAdd = () => {
         const div = document.createElement('div')
         div.innerHTML = String(index++ + 1)
         dom.appendChild(div)
-        _run()
         index >= count.value && (time.value = Date.now() - t)
+        current.value = index
+        _run()
       }
     })
   }
@@ -59,4 +66,44 @@ const onPerAdd = () => {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" module>
+.animate {
+  height: 100px;
+  overflow: hidden;
+  position: relative;
+
+  .dot {
+    $size: 20px;
+    width: $size;
+    height: $size;
+    position: absolute;
+    animation: dotMove 2s linear infinite;
+    // animation-timing-function: cubic-bezier(1, -0.51, 0.16, 1.52);
+    border-radius: 50%;
+    background-color: #f00;
+  }
+
+  @keyframes dotMove {
+    0% {
+      top: 75px;
+      left: 0;
+    }
+    25% {
+      top: 10px;
+      left: 25%;
+    }
+    50% {
+      top: 75px;
+      left: 50%;
+    }
+    75% {
+      top: 10px;
+      left: 75%;
+    }
+    100% {
+      top: 75px;
+      left: 100%;
+    }
+  }
+}
+</style>
