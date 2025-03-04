a5530bfad604e5e5272eaf50a72ef42d1fe0062c
evan-zl <evan2016@vip.qq.com>
2025-03-04 14:12:33 +0800

    fix:加载组件添加进度背景

--- code/src/components/loading/index.d.ts
+++ code/src/components/loading/index.d.ts
@@ -1,4 +1,5 @@
 export declare interface Props {
   bgColor?: string | number
   progress: number
+  bgSrc?: string
 }
--- code/src/components/loading/index.vue
+++ code/src/components/loading/index.vue
@@ -1,16 +1,22 @@
 <template>
   <div
     class="loading"
-    :class="$style.loading"
-    :style="{ '--bg-color': bgColor ? String(bgColor) : '' }"
+    :class="$style.wrap"
+    :style="{
+      '--bg-color': bgColor ? String(bgColor) : '',
+      '--bg-filter': (100 - progress) / 10 + 'px'
+    }"
     @dblclick.stop
     v-if="modelValue"
   >
-    <div :class="$style.progress" :style="{ '--percentage': progress + '%' }">
-      <div :class="$style['bar-out']">
-        <div :class="$style.bar"></div>
+    <img v-if="bgSrc" :src="bgSrc" :class="$style.bg" />
+    <div :class="$style.loading">
+      <div :class="$style.progress" :style="{ '--percentage': progress + '%' }">
+        <div :class="$style['bar-out']">
+          <div :class="$style.bar"></div>
+        </div>
+        <div :class="$style.text">{{ progress }}%</div>
       </div>
-      <div :class="$style.text">{{ progress }}%</div>
     </div>
   </div>
 </template>
